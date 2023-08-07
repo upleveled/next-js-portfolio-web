6026e0536d8a3f925e37739ae3b221a921f008ae
Lukas Prochazka <prochazka.lu@gmail.com>
2023-08-07 12:12:03 +0200

    Switch from Cypress to Playwright

---
+++ playwright/navigation.spec.ts
@@ -0,0 +1,30 @@
+import { expect, test } from '@playwright/test';
+
+test('Can find the different components in the app', async ({ page }) => {
+  await page.goto('/');
+  await expect(page.getByTestId('homePageTitle')).toBeVisible();
+  await expect(page.getByTestId('homePageLink')).toBeVisible();
+  await expect(page.getByTestId('portfolioPageLink')).toBeVisible();
+  await expect(page.getByTestId('contactPageLink')).toBeVisible();
+
+  await Promise.all([
+    page.getByTestId('aboutPageLink').click(),
+    page.waitForNavigation(),
+  ]);
+  await expect(page.url()).toContain('/about');
+  await expect(page.getByTestId('pageTitle')).toContainText('About');
+
+  await Promise.all([
+    page.getByTestId('portfolioPageLink').click(),
+    page.waitForNavigation(),
+  ]);
+  await expect(page.url()).toContain('/portfolio');
+  await expect(page.getByTestId('pageTitle')).toContainText('Portfolio');
+
+  await Promise.all([
+    page.getByTestId('contactPageLink').click(),
+    page.waitForNavigation(),
+  ]);
+  await expect(page.url()).toContain('/contact');
+  await expect(page.getByTestId('pageTitle')).toContainText('Contact');
+});
