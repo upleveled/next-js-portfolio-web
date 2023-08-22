6996fb0d93ff3c39ca434165ed703efc60f66153
Lukas Prochazka <prochazka.lu@gmail.com>
2023-08-22 13:16:00 +0200

    Change line breaks

--- playwright/navigation.spec.ts
+++ playwright/navigation.spec.ts
@@ -2,7 +2,6 @@ import { expect, test } from '@playwright/test';
 
 test('Can find the different components in the app', async ({ page }) => {
   await page.goto('/');
-
   await expect(page.getByTestId('homePageTitle')).toBeVisible();
   await expect(page.getByTestId('homePageLink')).toBeVisible();
   await expect(page.getByTestId('portfolioPageLink')).toBeVisible();
@@ -10,19 +9,16 @@ test('Can find the different components in the app', async ({ page }) => {
 
   await page.getByTestId('aboutPageLink').click();
   await page.waitForURL('/about');
-
   await expect(page).toHaveTitle('About');
   await expect(page.getByTestId('pageTitle')).toHaveText('About');
 
   await page.getByTestId('portfolioPageLink').click();
   await page.waitForURL('/portfolio');
-
   await expect(page).toHaveTitle('Portfolio');
   await expect(page.getByTestId('pageTitle')).toHaveText('Portfolio');
 
   await page.getByTestId('contactPageLink').click();
   await page.waitForURL('/contact');
-
   await expect(page).toHaveTitle('Contact');
   await expect(page.getByTestId('pageTitle')).toHaveText('Contact');
 });
