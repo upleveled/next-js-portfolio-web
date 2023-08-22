de83cd8c19c52c480d24657f9c04d2810ff99c7f
Lukas Prochazka <prochazka.lu@gmail.com>
2023-08-22 10:04:39 +0200

    Update page title test

--- playwright/navigation.spec.ts
+++ playwright/navigation.spec.ts
@@ -10,15 +10,21 @@ test('Can find the different components in the app', async ({ page }) => {
   await page.getByTestId('aboutPageLink').click();
   await page.waitForURL('/about');
 
+  await expect(page).toHaveTitle('About');
+
   await expect(page.getByTestId('pageTitle')).toHaveText('About');
 
   await page.getByTestId('portfolioPageLink').click();
   await page.waitForURL('/portfolio');
 
+  await expect(page).toHaveTitle('Portfolio');
+
   await expect(page.getByTestId('pageTitle')).toHaveText('Portfolio');
 
   await page.getByTestId('contactPageLink').click();
   await page.waitForURL('/contact');
+
+  await expect(page).toHaveTitle('Contact');
 
   await expect(page.getByTestId('pageTitle')).toHaveText('Contact');
 });
