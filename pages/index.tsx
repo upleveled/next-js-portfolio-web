c5c6d8bdeaed8a0b1d54cfb3ce01b51a8ba7cc6c
Lukas Prochazka <prochazka.lu@gmail.com>
2024-05-04 11:21:21 +0200

    Add project

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ function Home() {
         <div className="container">
           <div className="text-wrapper">
             <h1 data-test-id="pageTitle" className="title">
-              Halo, saya Lutfhi Ramadhan
+              Hi, my name is Lukas
             </h1>
             <p className="description">
               Saya seorang pekerja lepas pengembang web, fotograper, desainer
