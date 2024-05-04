7d9450b82867af09d990e1b69f21f29dfff623cf
Lukas Prochazka <prochazka.lu@gmail.com>
2024-05-04 11:24:35 +0200

    Revert "Add project"
    This reverts commit c5c6d8bdeaed8a0b1d54cfb3ce01b51a8ba7cc6c.

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ function Home() {
         <div className="container">
           <div className="text-wrapper">
             <h1 data-test-id="pageTitle" className="title">
-              Hi, my name is Lukas
+              Halo, saya Lutfhi Ramadhan
             </h1>
             <p className="description">
               Saya seorang pekerja lepas pengembang web, fotograper, desainer
